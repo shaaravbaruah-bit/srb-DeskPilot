8b26bf0eb0b9e98ec91ee7af4024e1503f9cb0ae
shaaravbaruah-bit <shaaravbaruah@gmail.com>
2026-03-21 14:36:08 +0530

    Added custom command system with file persistence

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,82 @@
 #include <iostream>
-#include <string>
-#include "../include/commands.h"
-
+#include <fstream>
+#include <map>
 using namespace std;
 
-int main(){
+map<string, string> commands;
 
-    string command;
+// Load commands from file
+void loadCommands() {
+    ifstream file("commands.txt");
 
-    cout<<"===== DeskPilot =====\n";
-
-    while(true){
-
-        cout<<"\nDeskPilot > ";
-        getline(cin, command);
-
-        if(command == "exit"){
-            cout<<"Closing DeskPilot...\n";
-            break;
-        }
-
-        executeCommand(command);
+    if (!file) {
+        cout << "Error: commands.txt not found!\n";
+        return;
     }
 
+    string line;
+
+    while (getline(file, line)) {
+        int pos = line.find('=');
+
+        if (pos == string::npos) continue;
+
+        string key = line.substr(0, pos);
+        string value = line.substr(pos + 1);
+
+        commands[key] = value;
+    }
+}
+
+// Add new command
+void addCommand(string input) {
+    string rest = input.substr(4);
+
+    int pos = rest.find(' ');
+    if (pos == string::npos) {
+        cout << "❌ Invalid format\n";
+        return;
+    }
+
+    string key = rest.substr(0, pos);
+    string value = rest.substr(pos + 1);
+
+    // Save to file
+    ofstream file("commands.txt", ios::app);
+    file << key << "=" << value << endl;
+
+    // Update map instantly
+    commands[key] = value;
+
+    cout << "✅ Command added successfully\n";
+}
+
+int main() {
+    loadCommands();
+
+    string input;
+
+    cout << "🔥 DeskPilot Started (type 'exit' to quit)\n";
+
+    while (true) {
+        cout << "DeskPilot > ";
+        getline(cin, input);
+
+        if (input == "exit") break;
+
+        // ADD COMMAND FEATURE
+        if (input.rfind("add ", 0) == 0) {
+            addCommand(input);
+        }
+        // RUN COMMAND
+        else if (commands.find(input) != commands.end()) {
+            system(commands[input].c_str());
+        }
+        else {
+            cout << "❌ Command not found\n";
+        }
+    }
+
+    cout << "Exiting DeskPilot...\n";
     return 0;
 }
